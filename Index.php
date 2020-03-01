fe373a61ccaeeef52eaf6f8de6ad986236aba164
Mo Xuan <c160011@e.ntu.edu.sg>
2020-03-01 11:10:33 +0800

    include db access

--- Index.php
+++ Index.php
@@ -1,4 +1,20 @@
-<?php?>
+<?php
+    include_once 'connect.php';
+
+    //check if form is submitted
+    if(isset($_POST['login'])){
+        $NRIC = mysqli_real_escape_string($conn,$_POST['NRIC']);
+        $password = mysqli_real_escape_string($conn,$_POST['password']);
+        $result=mysqli_query($conn,"SELECT * FROM patient WHERE NRIC = '" . $NRIC. "' and password = '" . $password . "'");
+        if ($row = mysqli_fetch_array($result)){
+            $_SESSION['NRIC'] = $row['NRIC'];
+        } else {
+            $errormsg="Incorrect NRIC or Password. Please try again.";
+        }
+    }
+
+    include_once 'dc.php';
+?>
 <html>
 
 
@@ -21,6 +37,7 @@
     <span class="fpw"> <a href="ForgetPW.php">Forget Password?</a></span>
 
     <!-- Login Button -->
+    <!-- login button use name 'login' for php authentication-->
     <form action="Main.php">
         <button type="submit" class="login">Login</button>
     </form>
