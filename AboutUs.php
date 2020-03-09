e844bfbe692b2ac9b3edf1a28c9390a215c3ddb7
Tyea <spartashen@hotmail.com>
2020-03-09 12:16:34 +0800

    Progressing AboutUs.php

--- AboutUs.php
+++ AboutUs.php
@@ -1,55 +1,29 @@
 <?php
 include('header.php');
 ?>
-
 <html>
-    <!-- resizing-->
-    <link rel="stylesheet" href="css/frame.css">
-    
-    <!-- Team_Logo -->
-    <img src="Assets/Img/CoronaSG_Logo.png" alt="Team_Logo" class="Team_Logo">
-
-
-    <!-- Title -->
-    <p1>Team Leader<p1>
-    <!-- Manager_img -->
-    <img src="Assets/Img/Temp.png" alt="Manager_img" class="Manager">
-    <!-- Description -->
-    <p1> Hahaha0 </p1>
-    
-
-    <!-- Title2 -->
-    <p1>Developer Division<p1>
-    <!-- Lead_img -->
-    <img src="Assets/Img/Temp.png" alt="Lead_img" class="Lead">
-    <!-- Description -->
-    <p1> Hahaha1 </p1>
-
-    <!-- Frontend_img -->
-    <img src="Assets/Img/Temp.png" alt="Frontend_img" class="Frontend">
-    <!-- Description -->
-    <p1> Hahaha2 </p1>
-    
-    <!-- Backend_img -->
-    <img src="Assets/Img/Temp.png" alt="Backend_img" class="Backend">
-    <!-- Description -->
-    <p1> Hahaha3 </p1>
-    
-
-    <!-- Title3 -->
-    <p1>Quality & Tester Division<p1>
-    <!-- QA_img -->
-    <img src="Assets/Img/Temp.png" alt="QA_img" class="QA">
-    <!-- Description -->
-    <p1> Hahaha4 </p1>
-
-    <!-- Test_img -->
-    <img src="Assets/Img/Temp.png" alt="Test_img" class="Test">
-    <!-- Description -->
-    <p1> Hahaha5 </p1>
-
-    <!-- Team Description -->
-    <p1>(Team Description)<p1>
+    <head>
+        <title>About Us</title>
+        <link rel="stylesheet" href="css/frame.css">
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/headfoot.css">
+        <link rel="stylesheet" href="css/layout.css">
+        <link rel="stylesheet" href="/external/fontawesome-free-5.12.1-web/css/all.css">
+        <meta name="viewport" content="width=device-width, initial-scale=1">   
+    </head>
+    <body>
+    <div class = "bgrd">
+        <div class="limiter">
+            <div class="container">
+                <div class="wrap_AboutUs">
+                </div>
+            </div>
+        </div>
+    </div>
+    </body>
+    <script src= "/external/jquery/jquery-3.4.1.js"></script> 
+    <script type= "text/javascript" src="js/bgrd.js"></script>
+</html>
 
 
 
