e92fe3fdf2080b0220dd363b773ae22c27b7c466
Tyea <spartashen@hotmail.com>
2020-03-09 18:00:45 +0800

    Main.php Completed

--- header.php
+++ header.php
@@ -15,7 +15,12 @@
                     <a href="_Front-End_Beta/Setting.php">
                         <i class="fas fa-3x fa-cog"></i>
                     </a>
+                    <a href="Index.php">
+                        <i class="fas fa-3x fa-sign-out-alt"></i>
+                    </a>
                 </span>
+
+                
             </div>
         </div>
     </header>
