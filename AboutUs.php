423760f81da3dcdf122243afc9b0b32d0142f520
Tyea <spartashen@hotmail.com>
2020-03-09 17:25:50 +0800

    AboutUs.php Complete

--- AboutUs.php
+++ AboutUs.php
@@ -12,7 +12,7 @@ include('header.php');
         <meta name="viewport" content="width=device-width, initial-scale=1">   
     </head>
     <body>
-    <div class = "bgrd">
+    <div class = "bgrd2">
         <div class="limiter">
             <div class="container">
                 <div class="wrap_AboutUs">
@@ -25,7 +25,7 @@ include('header.php');
                             <div class = "AboutUs_LeaderTitle"><h1>Team Leader</h1></div>
                             <div class = "AboutUs_LeaderImg"> <img src="Assets/Img/Temp.png" alt="Manager_img" class="Manager"></div>
                             <div class = "AboutUs_LeaderRole"><h3>Project Manager<h3></div>
-                            <div class = "AboutUs_LeaderDec"><p1> Hahaha0 </p1></div>
+                            <div class = "AboutUs_LeaderDec"><p1> Hahaha1 If you were to then give your CSS classes alignleft and alignright values of text-align: left; and text-align: right; respectively, you would get close to your desired result, but your right-aligned text would be bumped down one line because of the new paragraph. Instead, just float your paragraphs: </p1></div>
                         </span>
                         <span class = "AboutUs_UpRight">
                             <div class = "AboutUs_TeamDec"><h3>Hello</h3><p1>(Team Description)Social Problem or Is It Nonsense. One day on Twitter, I decided to type the word “colorism” in the search bar to see what people were discussing about it online. I came across a tweet that said “Yeah, I don 't believe in the whole colorism thing. Black is black to the general population. Y 'all just finding something to complain about.” I was shocked when I read it, especially when it was coming from a young, African American adult. I continued searching words like “dark-skin” and “light-skin, and found that in many of the tweets, members of the black community were referring to themselves as “light-skins”, but for what reason....   [tags: African American, Black people, Race]<p1></div>
@@ -39,15 +39,15 @@ include('header.php');
                                 <span class = "AboutUs_DevFront"><img src="Assets/Img/Temp.png" alt="Frontend_img" class="Frontend"></span>
                                 <span class = "AboutUs_DevBack"><img src="Assets/Img/Temp.png" alt="Backend_img" class="Backend"></span>
                                 </div>
-                            <div class = "AboutUs_DevTitle">
-                                <span class = "AboutUs_DevLeadT"><h3>Lead<h3></span>
-                                <span class = "AboutUs_DevFrontT"><h3>Front-End<h3></span>
-                                <span class = "AboutUs_DevBackT"><h3>Back-End<h3></span>
+                            <div class = "AboutUs_DevRole">
+                                    <p1 class = "AboutUs_DevLeadT" >Lead</p1>
+                                    <p1 class = "AboutUs_DevFrontT">Front-End</p1>
+                                    <p1 class = "AboutUs_DevBackT">Back-End</p1>
                             </div>
                             <div class = "AboutUs_DevDec">
-                                <span class = "AboutUs_DevLeadD"><p1> Hahaha1 </p1></span>
-                                <span class = "AboutUs_DevFrontD"><p1> Hahaha2 </p1></span>
-                                <span class = "AboutUs_DevBackD"><p1> Hahaha3 </p1></span>
+                                <p1 class = "AboutUs_DevLeadD"> Hahaha1 If you were to then give your CSS classes alignleft and alignright values of text-align: left; and text-align: right; respectively, you would get close to your desired result, but your right-aligned text would be bumped down one line because of the new paragraph. Instead, just float your paragraphs:</p1>
+                                <p1 class = "AboutUs_DevFrontD"> ; respectively, you would get close to your desired result, but your right-aligned text would be bumped down one line because of the new paragraph. Instead, just float your paragraphs: </p1>
+                                <p1 class = "AboutUs_DevBackD"> Hahaha3 If you were to then give your CSS classes alignleft and alignright values of text-align: left; and text-align: right; respectively, you would get close to your desired result, but your right-aligned text would be bumped down one line because of the new paragraph. Instead, just float your paragraphs:</p1>
                             </div>
                         </span>
                         <span class = "AboutUs_LowerMiddle">
@@ -59,12 +59,12 @@ include('header.php');
                                 <span class = "AboutUs_DocTestImg"><img src="Assets/Img/Temp.png" alt="Test_img" class="Test"></span>
                             </div>
                             <div class = "AboutUs_DocTitle">
-                                <span class = "AboutUs_DocQAT"><h3>QA Quality<h3></span>
-                                <span class = "AboutUs_DocTestT"><h3>QA Tester<h3></span>
+                                <p1 class = "AboutUs_DocQAT">QA Quality</p1>
+                                <p1 class = "AboutUs_DocTestT">QA Tester</p1>
                             </div>
                             <div class = "AboutUs_DocDec">
-                                <span class = "AboutUs_DocQAD"><p1> Hahaha4 </p1></span>
-                                <span class = "AboutUs_DocTestD"><p1> Hahaha5 </p1></span>
+                                <p1 class = "AboutUs_DocQAD"> Hahaha1 If you were to then give your CSS classes alignleft and alignright values of text-align: left; and text-align: right; respectively, you would get close to your desired result, but your right-aligned text would be bumped down one line because of the new paragraph. Instead, just float your paragraphs: </p1>
+                                <p1 class = "AboutUs_DocTestD"> Hahaha1 If you were to then give your CSS classes alignleft and alignright values of text-align: left; and text-align: right; respectively, you would get close to your desired result, but your right-aligned text would be bumped down one line because of the new paragraph. Instead, just float your paragraphs:</p1>
                             </div>
                         </span>
                     </div>
