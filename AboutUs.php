c5afc61ecd0ca2a8b2b42de39f5b7f22293799d3
Tyea <spartashen@hotmail.com>
2020-03-09 14:03:55 +0800

    AboutUs.php Progression

--- AboutUs.php
+++ AboutUs.php
@@ -16,6 +16,58 @@ include('header.php');
         <div class="limiter">
             <div class="container">
                 <div class="wrap_AboutUs">
+                    <div class = "AboutUs_Upper">
+                        <span class = "AboutUs_UpLeft">
+                            <div class = "AboutUs_Logo"><img src="Assets/Img/CoronaSG_Logo.png" alt="Team_Logo" class="Team_Logo"></div>
+                            <div class = "AboutUs_DevTitle"><h1>Developer Division</h1></div>
+                        </span>
+                        <span class = "AboutUs_UpMiddle">
+                            <div class = "AboutUs_LeaderTitle"><h1>Team Leader</h1></div>
+                            <div class = "AboutUs_LeaderImg"> <img src="Assets/Img/Temp.png" alt="Manager_img" class="Manager"></div>
+                            <div class = "AboutUs_LeaderRole"><h3>Project Manager<h3></div>
+                            <div class = "AboutUs_LeaderDec"><p1> Hahaha0 </p1></div>
+                        </span>
+                        <span class = "AboutUs_UpRight">
+                            <div class = "AboutUs_TeamDec"><h3>Hello</h3><p1>(Team Description)Social Problem or Is It Nonsense. One day on Twitter, I decided to type the word “colorism” in the search bar to see what people were discussing about it online. I came across a tweet that said “Yeah, I don 't believe in the whole colorism thing. Black is black to the general population. Y 'all just finding something to complain about.” I was shocked when I read it, especially when it was coming from a young, African American adult. I continued searching words like “dark-skin” and “light-skin, and found that in many of the tweets, members of the black community were referring to themselves as “light-skins”, but for what reason....   [tags: African American, Black people, Race]<p1></div>
+                            <div class = "AboutUs_DocTitle"><h1>Quality & Tester Division<h1></div>
+                        </span>
+                    </div>
+                    <div class = "AboutUs_Lower">
+                        <span class = "AboutUs_LowerLeft">
+                            <div class = "AboutUs_DevImg">
+                                <span class = "AboutUs_DevLead"><img src="Assets/Img/Temp.png" alt="Lead_img" class="Lead"></span>
+                                <span class = "AboutUs_DevFront"><img src="Assets/Img/Temp.png" alt="Frontend_img" class="Frontend"></span>
+                                <span class = "AboutUs_DevBack"><img src="Assets/Img/Temp.png" alt="Backend_img" class="Backend"></span>
+                                </div>
+                            <div class = "AboutUs_DevTitle">
+                                <span class = "AboutUs_DevLeadT"><h3>Lead<h3></span>
+                                <span class = "AboutUs_DevFrontT"><h3>Front-End<h3></span>
+                                <span class = "AboutUs_DevBackT"><h3>Back-End<h3></span>
+                            </div>
+                            <div class = "AboutUs_DevDec">
+                                <span class = "AboutUs_DevLeadD"><p1> Hahaha1 </p1></span>
+                                <span class = "AboutUs_DevFrontD"><p1> Hahaha2 </p1></span>
+                                <span class = "AboutUs_DevBackD"><p1> Hahaha3 </p1></span>
+                            </div>
+                        </span>
+                        <span class = "AboutUs_LowerMiddle">
+
+                        </span>
+                        <span class = "AboutUs_LowerRight">
+                            <div class = "AboutUs_DocImg">
+                                <span class = "AboutUs_DocQAImg"><img src="Assets/Img/Temp.png" alt="QA_img" class="QA"></span>
+                                <span class = "AboutUs_DocTestImg"><img src="Assets/Img/Temp.png" alt="Test_img" class="Test"></span>
+                            </div>
+                            <div class = "AboutUs_DocTitle">
+                                <span class = "AboutUs_DocQAT"><h3>QA Quality<h3></span>
+                                <span class = "AboutUs_DocTestT"><h3>QA Tester<h3></span>
+                            </div>
+                            <div class = "AboutUs_DocDec">
+                                <span class = "AboutUs_DocQAD"><p1> Hahaha4 </p1></span>
+                                <span class = "AboutUs_DocTestD"><p1> Hahaha5 </p1></span>
+                            </div>
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -24,11 +76,6 @@ include('header.php');
     <script src= "/external/jquery/jquery-3.4.1.js"></script> 
     <script type= "text/javascript" src="js/bgrd.js"></script>
 </html>
-
-
-
-</html>
-
 <?php
 include('footer.php');
 ?>
