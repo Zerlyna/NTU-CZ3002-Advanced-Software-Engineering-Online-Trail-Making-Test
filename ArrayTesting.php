4b8868e715acb42ad7d2c6360e05cad957e1537f
Tyea <spartashen@hotmail.com>
2020-03-10 19:50:56 +0800

    Organize and Remove (Non-Core) Files

--- ArrayTesting.php
+++
@@ -1,7 +0,0 @@
-<?php?>
-<html>
-<body>
-<button class="btnStart" onclick="storeUserData()" >Test</button>
-<script type="text/javascript" src="arrayTesting.js"></script>
-</body>
-</html>
--- GraphTesting.php
+++
@@ -1,9 +0,0 @@
-<?php?>
-<html>
-
-<body>
-<div id="chartContainer" style="height: 370px; width: 100%;"></div>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-<script src="chartTesting.js"></script>
-</body>
-</html>
