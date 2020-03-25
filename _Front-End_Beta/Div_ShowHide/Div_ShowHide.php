84b83cdc1757898a4381246fa86a6748b6691504
Tyea <spartashen@hotmail.com>
2020-03-25 18:31:51 +0800

    Delete Front_End_Beta Folder, Visual Changes

--- _Front-End_Beta/Div_ShowHide/Div_ShowHide.php
+++
@@ -1,8 +0,0 @@
-<div class="topdiv" *ngIf="!blockContent"> 
-     <button>Cancel</button>
-</div>
-<div class="divBlocking" *ngIf="blockContent"></div>
-
-<div class="divApp"> 
-     HAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
-</div>
